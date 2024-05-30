b9146ec1277f5c820db5fc5b07ed0ac886d5164d
Carlo10K <carlosortegadiaz50@gmail.com>
2024-05-30 09:41:32 -0600

    filtro blur a los items de la tabla

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CardTable extends StatelessWidget {
@@ -45,32 +47,52 @@ class _SingleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return _CardBackground(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          backgroundColor: color,
+          radius: 30,
+          child: Icon(
+            icon,
+            size: 35,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          text,
+          style: TextStyle(color: color, fontSize: 18),
+        )
+      ],
+    ));
+  }
+}
+
+class _CardBackground extends StatelessWidget {
+  final Widget child;
+
+  const _CardBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(15),
-      height: 180,
-      decoration: BoxDecoration(
-          color: const Color.fromRGBO(62, 66, 107, 0.7),
-          borderRadius: BorderRadius.circular(20)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            backgroundColor: color,
-            radius: 30,
-            child: Icon(
-              icon,
-              size: 35,
-              color: Colors.white,
-            ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Container(
+            height: 180,
+            decoration: BoxDecoration(
+                color: const Color.fromRGBO(62, 66, 107, 0.7),
+                borderRadius: BorderRadius.circular(20)),
+            child: child,
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          Text(
-            text,
-            style: TextStyle(color: color, fontSize: 18),
-          )
-        ],
+        ),
       ),
     );
   }
